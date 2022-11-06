27f84068410ba605d86e28fde1b90f38b3761966
lightxlamp <sheveliukhin.s@gmail.com>
2022-11-07 01:41:29 +0600

    study: check global variables

---
+++ Global-variables/app.js
@@ -0,0 +1,6 @@
+console.log("\n", "__dirname", __dirname, "\n");
+console.log("__filename", __filename, "\n");
+console.log("global", global, "\n");
+console.log("performance", performance, "\n");
+console.log("module", module, "\n");
+console.log("require", require, "\n");
