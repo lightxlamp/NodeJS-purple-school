832380a27535fadb1357924811adc08c0ff80226
lightxlamp <sheveliukhin.s@gmail.com>
2022-11-06 13:59:13 +0600

    study: Async import of modules

---
+++ ES-Modules-Async-LOTR/characters.mjs
@@ -0,0 +1,13 @@
+export const characters = ["Frodo", "Sam"];
+
+export function greet(character) {
+  console.log("");
+  console.log("=== Congratulations " + character + "! ===");
+  console.log("");
+}
+
+export default function log() {
+  console.log("");
+  console.log("~~~ Export default logging function ~~~");
+  console.log("");
+}
