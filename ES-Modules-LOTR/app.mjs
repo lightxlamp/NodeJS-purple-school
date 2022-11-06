eb708169e6d61ae9fa017e19476a352d29c32bb7
lightxlamp <sheveliukhin.s@gmail.com>
2022-11-06 13:09:59 +0600

    study: another way of importing ES Module

--- ES-Modules-LOTR/app.mjs
+++ ES-Modules-LOTR/app.mjs
@@ -1,5 +1,10 @@
-import { characters, greet } from "./characters.mjs";
+import * as char from "./characters.mjs";
+// import { characters, greet } from "./characters.mjs";
 
-for (const c of characters) {
-  greet(c);
+for (const c of char.characters) {
+  char.greet(c);
 }
+
+// for (const c of characters) {
+//   greet(c);
+// }
