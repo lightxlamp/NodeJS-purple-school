7efe7547a79e350db020a4a0c07cea0021a6278a
lightxlamp <sheveliukhin.s@gmail.com>
2022-11-06 13:25:44 +0600

    study: 3 ways of importing modules

--- ES-Modules-LOTR/app.mjs
+++ ES-Modules-LOTR/app.mjs
@@ -1,5 +1,6 @@
 import * as char from "./characters.mjs";
 // import { characters, greet } from "./characters.mjs";
+import someDefault from "./characters.mjs";
 
 for (const c of char.characters) {
   char.greet(c);
@@ -8,3 +9,21 @@ for (const c of char.characters) {
 // for (const c of characters) {
 //   greet(c);
 // }
+
+someDefault();
+
+char.default();
+
+console.log(char);
+
+console.log(".......");
+
+// The third was =D
+
+import log, { characters, greet } from "./characters.mjs";
+
+log();
+
+for (const c of characters) {
+  greet(c);
+}
--- ES-Modules-LOTR/characters.mjs
+++ ES-Modules-LOTR/characters.mjs
@@ -1,5 +1,13 @@
 export const characters = ["Frodo", "Sam"];
 
 export function greet(character) {
-  console.log("Congratulations " + character + "!");
+  console.log("");
+  console.log("=== Congratulations " + character + "! ===");
+  console.log("");
+}
+
+export default function log() {
+  console.log("");
+  console.log("~~~ Export default logging function ~~~");
+  console.log("");
 }
