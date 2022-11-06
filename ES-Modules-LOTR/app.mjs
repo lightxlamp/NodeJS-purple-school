b34958836f1946f0b09dcfca2d579adb53c30f48
lightxlamp <sheveliukhin.s@gmail.com>
2022-11-06 13:07:36 +0600

    study: ES modules

---
+++ ES-Modules-LOTR/app.mjs
@@ -0,0 +1,5 @@
+import { characters, greet } from "./characters.mjs";
+
+for (const c of characters) {
+  greet(c);
+}
---
+++ ES-Modules-LOTR/characters.mjs
@@ -0,0 +1,5 @@
+export const characters = ["Frodo", "Sam"];
+
+export function greet(character) {
+  console.log("Congratulations " + character + "!");
+}
