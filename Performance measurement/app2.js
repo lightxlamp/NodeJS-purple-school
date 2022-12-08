d47feed10df2a60a9dd2ad7ad4d3bab8a2939d33
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-12-08 23:44:44 +0600

    study: finish lesson

--- Performance measurement/app2.js
+++ Performance measurement/app2.js
@@ -1,12 +1,21 @@
 const perf_hooks = require('perf_hooks')
 const performanceObserver = new perf_hooks.PerformanceObserver((items, observer) => {
-    // console.log(items.getEntries())
+    console.log(items.getEntries())
     const entry = items.getEntriesByName('slowFunctionMeasurement').pop()
     console.log(`${entry.name}: ${entry.duration}`);
     observer.disconnect();
 });
 
-performanceObserver.observe({ entryTypes: ['measure'] })
+performanceObserver.observe({ entryTypes: ['measure', 'function'] })
+
+function test() {
+    const arr = [];
+    for (let i = 0; i < 100000000; i++) {
+        arr.push(i * i)
+    }
+}
+
+test = perf_hooks.performance.timerify(test) // "wrap" function with functionality measurement  
 
 function slow() {
     performance.mark('start')
@@ -19,3 +28,4 @@ function slow() {
 }
 
 slow()
+test()
